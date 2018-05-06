2ae3641ba90b00f7aafac08c5f567ba9dcbb3c97
liuhongbo <liuhongbo@finance.com>
2018-05-06 16:49:51 +0800

    网络请求
    首页的网络请求

--- wxDemo/pages/index/index.js
+++ wxDemo/pages/index/index.js
@@ -1,6 +1,6 @@
 // pages/index/index.js
 // 值为gn、gj、cj、yl、js、ty和other其中之一
-
+var utils = require('../../utils/util.js')
 const h_btn_flag_arr = [
   "gn",
   "gj",
@@ -27,81 +27,75 @@ Page({
   data: {
     navigationItems: h_btn_text_arr,
     currentTab: 0,
-    newsList: [{
-      "id": 1523074607642,
-      "title": "清明假期长三角铁路客流堪比春运 上海局增开客车124列",
-      "date": "02:01",
-      "source": "source",
-      "firstImage": "http://inews.gtimg.com/newsapp_bt/0/3203388080/641"},
-      {
-        "id": 1523074607642,
-        "title": "清明假期长三角铁路客流堪比春运 上海局增开客车124列",
-        "date": "02:01",
-        "source": "source",
-        "firstImage": "http://inews.gtimg.com/newsapp_bt/0/3203388080/641"
-      },
-      {
-        "id": 1523074607642,
-        "title": "清明假期长三角铁路客流堪比春运 上海局增开客车124列",
-        "date": "02:01",
-        "source": "source",
-        "firstImage": "http://inews.gtimg.com/newsapp_bt/0/3203388080/641"
-      },
-      {
-        "id": 1523074607642,
-        "title": "清明假期长三角铁路客流堪比春运 上海局增开客车124列",
-        "date": "02:01",
-        "source": "source",
-        "firstImage": "http://inews.gtimg.com/newsapp_bt/0/3203388080/641"
-      }]
+    newsList: [],
+    firstNews:{}
   },
   navbarTap: function (e) {
     console.debug(e);
     this.setData({
       currentTab: e.currentTarget.dataset.idx
     }) 
+    this.getNetworkDataAt(e.currentTarget.dataset.idx)
     },
+
+  getNetworkDataAt(index,callBlock){
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 10000
+    }),
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list?type=' + h_btn_flag_arr[index],
+      success: res => {
+        let result = res.data.result
+        console.log(result)
+        if (result && result.length){
+          // var arr_result = new Array()
+
+          // for (var item in result) {
+          //   var item_dict = {} 
+          //   let time = utils.formatTime(Number(item.date),'h:m:s')
+
+          //   item_dict.id = item.id;
+          //   item_dict.firstImage = item.firstImage
+          //   item_dict.date = item.time
+          //   item_dict.source = item.source
+          //   item_dict.title = item.title
+          //   arr_result.push(item_dict)
+          // }  
+
+          let first_new = result[0];
+          let news_list = result.slice(1,result.length)
+          this.setData({
+            newsList: news_list,
+            firstNews: first_new
+          })
+        }
+        
+        // this.setNow(result)
+        // this.setHourlyWeather(result)
+        // this.setToday(result)
+      },
+      complete: () => {
+        wx.hideToast()
+        callBlock && callBlock()
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getNetworkDataAt(0)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    this.getNetworkDataAt(this.currentTab,() => {
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
