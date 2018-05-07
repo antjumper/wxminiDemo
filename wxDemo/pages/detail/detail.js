e63d9b3a7b833a20c9a4f2200c19a5e37c97efc2
liuhongbo <liuhongbo@finance.com>
2018-05-07 12:04:27 +0800

    小程序 新闻详情界面
    完成详情界面

--- wxDemo/pages/detail/detail.js
+++ wxDemo/pages/detail/detail.js
@@ -23,12 +23,13 @@ Page({
     })
     this.getNewsDetail()
   },
- 
+  // 刷新
   onPullDownRefresh() {
     this.getNewsDetail(() => {
       wx.stopPullDownRefresh()
     })
   },
+  //获取网络数据
   getNewsDetail(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/detail?id='+this.data.id,
@@ -42,89 +43,25 @@ Page({
       }
     })
   },
+  //关联网络数据
   setDetailData(result){
-    var xmlString = ""
     let arr = result.content
-    console.log(arr)
-    for (var i = 0, len = arr.length; i < len; i++) {
-      let dic_item = arr[i]
-      let src = dic_item.src
-      let text = dic_item.text
-      xmlString = xmlString + "\r\n<" + dic_item.type
-      if (src){
-        xmlString = xmlString + " src=" + src
-      }
-      xmlString = xmlString + ">"
-
-      if (text){
-        xmlString = xmlString + text
-      }
-      xmlString = xmlString + "<\/" + dic_item.type + ">"
-      // xmlString = xmlString + "<" + dic_item.type + " src=" + dic_item.src
-      //   + " >"
-      //   + dic_item.text
-      //   + "</" + dic_item.type + ">"
-    }
-    // for (dic_item in arr){
-    //   console.log(xmlString)
-    //   xmlString = xmlString + "<" + dic_item.type + " src=" + dic_item.src
-    //     +" >" 
-    //     + dic_item.text
-    //     + "</" + dic_item.type+">"
-    // }
     this.setData({
       id: result.id,
       title: result.title,
       date: result.date,
       source: result.source,
       firstImage: result.firstImage,
-      content: xmlString,
+      content: arr,
       readCount: result.readCount
     })
 
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
   
+  onTapBack: function (e) {
+    console.debug(e);
+    wx.navigateBack()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
   /**
    * 用户点击右上角分享
    */
